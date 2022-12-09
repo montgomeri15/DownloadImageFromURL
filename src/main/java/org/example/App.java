285683b734030eb790c3c71e4625fdc29744e06c
montgomeri15 <montgomeri15@gmail.com>
2022-12-09 18:36:26 +0200

    With picture download and tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class App {
     public static void main( String[] args ) {
+        String imageUrl = "https://cs8.pikabu.ru/post_img/big/2016/04/13/8/1460549895152122999.jpg";
+        String dirName = "directory";
+        String fileName = "saitama.png";
 
+        DownloadImage.createDir(dirName);
+        DownloadImage.saveImage(imageUrl, dirName, fileName);
     }
 }
--- src/main/java/org/example/DownloadImage.java
+++ src/main/java/org/example/DownloadImage.java
@@ -4,10 +4,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class DownloadImage {
 
+    public static java.io.File createDir(String dirName) {
+        try {
+            return Files.createDirectory(Paths.get(dirName)).toFile();
+        } catch (IOException e) {
+            System.err.println(e);
+            return null;
+        }
+    }
 
+    public static void saveImage(String imageUrl, String dirName, String fileName) {
+        try(InputStream in = new URL(imageUrl).openStream()){
+            Files.copy(in, Paths.get(dirName, fileName));
+        } catch (IOException e) {
+            System.err.println(e);;
+        }
+    }
 }
